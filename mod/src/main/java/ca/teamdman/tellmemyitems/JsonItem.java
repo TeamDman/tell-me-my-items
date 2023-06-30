3351a8e306202dd14b882c5e4ec299b2ea84f5d6
TeamDman <TeamDman9201@gmail.com>
2023-06-29 22:03:33 -0400

    1.19.4 port

--- mod/src/main/java/ca/teamdman/tellmemyitems/JsonItem.java
+++ mod/src/main/java/ca/teamdman/tellmemyitems/JsonItem.java
@@ -1,14 +1,14 @@
 package ca.teamdman.tellmemyitems;
 
-import net.minecraft.core.Registry;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class JsonItem {
 	public String data;
 	public String id;
 
 	public JsonItem(ItemStack stack) {
-		this.id = Registry.ITEM.getKey(stack.getItem())
+		this.id = ForgeRegistries.ITEMS.getKey(stack.getItem())
 				.toString();
 		if (stack.getShareTag() != null) {
 			data = stack.getShareTag()
--- mod/src/main/java/ca/teamdman/tellmemyitems/TellMeMyItems.java
+++ mod/src/main/java/ca/teamdman/tellmemyitems/TellMeMyItems.java
@@ -2,11 +2,10 @@ package ca.teamdman.tellmemyitems;
 
 import com.google.gson.Gson;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.commands.Commands;
-import net.minecraft.core.NonNullList;
-import net.minecraft.core.Registry;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.event.RegisterClientCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,65 +19,77 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
 
 import static ca.teamdman.tellmemyitems.TellMeMyItems.MOD_ID;
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
 @Mod(MOD_ID)
 public class TellMeMyItems {
-	public static final String MOD_ID = "tellmemyitems";
+    public static final String MOD_ID = "tellmemyitems";
 
-	private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-	public TellMeMyItems() {}
+    public TellMeMyItems() {
+    }
 
-	public static void dumpItems() throws IOException {
-		var items   = gatherItems();
-		var gameDir = FMLPaths.GAMEDIR.get();
-		var folder  = Paths.get(gameDir.toString(), "tmmi");
+    public static void dumpItems() throws IOException {
+        var items = gatherItems();
+        var gameDir = FMLPaths.GAMEDIR.get();
+        var folder = Paths.get(gameDir.toString(), "tmmi");
 
-		// ensure folder exists
-		Files.createDirectories(folder);
+        // ensure folder exists
+        Files.createDirectories(folder);
 
-		// serialize items
-		var mappedItems = items.stream()
-				.map(JsonItem::new)
-				.toList();
-		Gson gson    = new Gson();
-		var  content = gson.toJson(mappedItems);
+        // serialize items
+        var mappedItems = items.stream()
+                .map(JsonItem::new)
+                .toList();
+        Gson gson = new Gson();
+        var content = gson.toJson(mappedItems);
 
-		// write to file
-		File itemFile = new File(folder.toFile(), "items.json");
-		try (FileOutputStream str = new FileOutputStream(itemFile)) {
-			str.write(content.getBytes(StandardCharsets.UTF_8));
-		}
-		LOGGER.info("Exported item data to {}", itemFile);
-	}
+        // write to file
+        File itemFile = new File(folder.toFile(), "items.json");
+        try (FileOutputStream str = new FileOutputStream(itemFile)) {
+            str.write(content.getBytes(StandardCharsets.UTF_8));
+        }
+        LOGGER.info("Exported item data to {}", itemFile);
+        Minecraft.getInstance().player.sendSystemMessage(Component.literal(String.format(
+                "Exported %d items to \"%s\"",
+                items.size(),
+                itemFile.getAbsolutePath()
+        )));
+    }
 
-	public static NonNullList<ItemStack> gatherItems() {
-		var items = NonNullList.<ItemStack>create();
-		for (Item item : Registry.ITEM) {
-			if (item == null) continue;
-			item.fillItemCategory(CreativeModeTab.TAB_SEARCH, items);
-		}
-		return items;
-	}
+    public static Collection<ItemStack> gatherItems() {
+        CreativeModeTabs.tryRebuildTabContents(
+                Minecraft.getInstance().player.connection.enabledFeatures(),
+                true,
+                Minecraft.getInstance().level.registryAccess()
+        );
+//		CreativeModeTabs.SEARCH.buildContents(new CreativeModeTab.ItemDisplayParameters(
+//				Minecraft.getInstance().player.connection.enabledFeatures(),
+//				true,
+//				Minecraft.getInstance().level.registryAccess()
+//		));
+        return CreativeModeTabs.SEARCH.getDisplayItems();
+    }
 
-	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
-	public static class RegistryEvents {
-		@SubscribeEvent
-		public static void onRegisterCommand(final RegisterClientCommandsEvent event) {
-			event.getDispatcher()
-					.register(Commands.literal("tmmiexport")
-									  .executes(context -> {
-										  LOGGER.info("Exporting data");
-										  try {
-											  dumpItems();
-										  } catch (IOException e) {
-											  LOGGER.error("Error dumping data", e);
-										  }
-										  return SINGLE_SUCCESS;
-									  }));
-		}
-	}
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onRegisterCommand(final RegisterClientCommandsEvent event) {
+            event.getDispatcher()
+                    .register(Commands.literal("tmmiexport")
+                                      .executes(context -> {
+                                          LOGGER.info("Exporting data");
+                                          try {
+                                              dumpItems();
+                                          } catch (IOException e) {
+                                              LOGGER.error("Error dumping data", e);
+                                          }
+                                          return SINGLE_SUCCESS;
+                                      }));
+        }
+    }
 }
